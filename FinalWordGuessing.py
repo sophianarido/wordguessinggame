816df80979dde13aad85fed5de7b2ca3d68ae0bd
sophianarido <sophiadazed@gmail.com>
2024-04-23 18:18:12 -0400

    Created Game Loop Shell

--- FinalWordGuessing.py
+++ FinalWordGuessing.py
@@ -21,4 +21,17 @@ def start():
                     break
             else:
                 print("That is not a word in the list. Please try again.")
-      
+
+# Game loop for player(s)
+num_players = int(input("Enter the number of players (1 or 2): "))
+word_list = ["PUDDING", "CAKE", "FLAN", "TIRAMISU", "BROWNIE"]
+secret_word = random.choice(word_list)
+word_length = len(secret_word)
+
+guesses = []
+max_guesses = 3
+
+
+for player in range(1, num_players + 1):
+        print(f"Player {player}'s turn:")
+        
